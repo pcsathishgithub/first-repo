41919d5f8f297a09a2190dd8d75b44e5716f53e5
Sathish P. C <sathishprithiviraj@gmail.com>
2025-11-11 13:17:20 +0530

    Models added for SQLite DB

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BookingTable(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=255, null=True)
+    noOfGuests = models.PositiveIntegerField(default=0)
+    bookingDate = models.DateField(null=True)
+
+class MenuTable(models.Model):
+    id = models.IntegerField
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    inventory = models.PositiveIntegerField(default=0)
